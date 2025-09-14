ffddbad46c47c6ce98d708ca3e3a870aca750045
Runner-Dad84 <vanhoogie@gmail.com>
2025-09-13 22:20:17 -0400

    add password validator

--- routes/newUserRouter.js
+++ routes/newUserRouter.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const newUserRouter = Router();
 const controller = require("../controllers/indexController");
+const controllerValidate = require("../controllers/validatorController");
 
 //render sign up form
 newUserRouter.get("/newUser", (req, res) => { res.render("newUser") });
 
 //sign up new user
-newUserRouter.post("/newUser", controller.signupUser)
+newUserRouter.post("/newUser", controllerValidate.verifyPassword, controller.signupUser)
 
 
 module.exports = newUserRouter;
