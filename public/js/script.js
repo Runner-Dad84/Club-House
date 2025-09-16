4c1aa3089d26cac9c19e4779c24ad2c8b1ec8d83
Runner-Dad84 <vanhoogie@gmail.com>
2025-09-15 21:06:29 -0400

    Move variable and prevent default

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
 const form = document.getElementById('newUserForm');
 form.addEventListener('submit', (e)=> {
-    e.preventDefault();
+    
+    const password = document.getElementById('password').value;
+    const verified = document.getElementById('validatePassword').value;
 
     if(password !== verified) {
-        const password = document.getElementById('password').value;
-        const verified = document.getElementById('validatePassword').value;
-        
+        e.preventDefault();
         console.log(password, verified)
-        alert('Passwords do not match!')
-    } else {
-        "Match!"
+        alert('Passwords do not match!');
     }
-    });
-})
+});
+});
+
 
